bd31c79fe2ac3d62b4c1cdc191509e36151412e6
Jack Kieny <jackkieny@cox.net>
2025-02-13 19:46:27 -0600

    feat: refactor layout and apply custom theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,31 @@
-import { Flex, Group, MantineProvider } from '@mantine/core'
+import { createTheme, Flex, MantineProvider } from '@mantine/core'
 import { Left } from './components/left/Left'
 import { Right } from './components/right/Right'
 import classes from './app.module.css'
 import '@mantine/core/styles.css'
 
+const theme = createTheme({
+  colors: {
+    primary: [
+      "#f2f5f8", "#e4e6e9", "#c5ccd3", "#a3b0be", "#8798ad",
+      "#202C39", "#1A1F26", "#14171D", "#0E1014", "#08090B"
+    ]
+  }
+})
+
 function App() {
 
   return (
-    <MantineProvider>
+    <MantineProvider theme={theme}>
       <div className={classes.container}>
-        <Group
-          justify='space-between'
-          align='flex-start'
-          w="60%"
-          className={classes.content}>
-
-          {/* Left - Header */}
-          <Flex
-            align='center'
-            w="20%"
-            direction='column'
-          >
-            <Left />
-          </Flex>
-
-          {/* Right - Main */}
-          <Group
-            justify='flex-start'
-            w="60%"
-          >
-            <Right />
-          </Group>
-        </Group>
+        <div className={classes.content}>
+          <div className={classes.left}>
+            <Left/>
+          </div>
+          <div className={classes.right}>
+            <Right/>
+          </div>
+        </div>
       </div>
     </MantineProvider>
   )
