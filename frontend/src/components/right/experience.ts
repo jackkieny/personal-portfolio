b38f8ed84426899e3a2899e3e0455f59693665ec
Jack Kieny <jackkieny@cox.net>
2025-02-18 20:28:28 -0600

    feat: add Experience component and update layout styles to fit content

---
+++ frontend/src/components/right/experience.ts
@@ -0,0 +1,19 @@
+export const experience = [
+    {
+        company: 'National Indemnity Company',
+        position: 'SQL Developer',
+        start: '2023',
+        end: 'DEC 2024',
+        description: 'Managed and maintained 15+ enterprise-wide applications, optimizing SQL queries and stored procedures for enhanced performance. Supported ETL processes to ensure accurate and efficient data integration within data warehouse applications. Worked collaboratively in an Agile Scrum environment to deliver high-quality solutions on schedule.',
+        tools: ['SQL', 'C#', 'VB.Net', 'Azure'],
+    },
+    {
+        company: 'Chick-fil-A',
+        position: 'Manager',
+        start: '2020',
+        end: '2023',
+        description: 'Committed to guest satisfaction, quality standards, and a sense of urgency. Managed a team of individuals to ensure that labor and productivity goals were met. Cash management, problem-solving skills, adaptive and independent.',
+        tools: [],
+    },
+]
+
