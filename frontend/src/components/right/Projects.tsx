79cd658d3b36f4d25baf182a6cabfcc46223458f
Jack Kieny <jackkieny@cox.net>
2025-02-19 19:26:43 -0600

    feat: add projects section

---
+++ frontend/src/components/right/Projects.tsx
@@ -0,0 +1,60 @@
+import classes from './styles/Projects.module.css';
+import { Flex, Text, Pill, Image } from '@mantine/core';
+import { projects } from './projects.ts';
+import { IconArrowUpRight } from '@tabler/icons-react';
+
+export function Projects() {
+
+  const ProjectsList = projects.map((proj, idx) => {
+    return (
+      <Flex
+        key={idx}
+        mb={25}
+        p={10}
+        className={classes.paper}
+        onMouseDown={(e) => {
+          e.preventDefault();
+          window.open(proj.link, '_blank')
+        }}
+      >
+        <Flex align='start' gap='md' className={classes.inner}>
+
+          <Flex className={classes.left} w="30%" direction='column' gap={10} align='center'>
+            <Image
+              src={proj.image}
+              alt={proj.name}
+              w={150}
+              h={100}
+              radius="md"
+            />
+            <Text c="dimmed" fz={13}>{proj.year}</Text>
+          </Flex>
+
+          <Flex className={classes.right} direction='column' w="75%">
+            <Text fz={18} fw={700}>{proj.name} <IconArrowUpRight size={16} className={classes.icon} /></Text>
+            <Text fz={14} c="dimmed">{proj.description}</Text>
+            <Flex gap={10} m={10}>
+              {proj.tools.map((tool, idx) => {
+                return (
+                  <Pill
+                    key={idx}
+                    fz={15}
+                    c={'#A3B0BE'}
+                    bg={'#202C39'}
+                    className={classes.pill}
+                  >{tool}</Pill>
+                )
+              })}
+            </Flex>
+          </Flex>
+        </Flex>
+      </Flex>
+    )
+  })
+
+  return (
+    <div className={classes.container}>
+      {ProjectsList}
+    </div>
+  )
+}
---
+++ frontend/src/components/right/projects.ts
@@ -0,0 +1,10 @@
+export const projects = [
+    {
+        image: '/schoolofengineering.png',
+        name: 'A New pyP3DB',
+        description: 'Redesigned and built a Python-based web framework for displaying protein modification data in a more flexible, comprehensive, and secure way. Served as the Product Manager and a Backend Engineer on the senior design team developing this full-stack web app. Led weekly meetings, tracked requirements, and applied Agile practices.',
+        year: `2021 \u2014 2022`,
+        link: 'https://computing.unl.edu/senior-design/showcase-projects-2021-22/#widget40',
+        tools: ['Python', 'Django', 'SQL', 'MongoDB', 'AWS'],
+    },
+]
