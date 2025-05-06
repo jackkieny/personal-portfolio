d47d837a2cb48bb39fd1523ac0a8bb4940b0ce9c
Jack <59969032+jackkieny@users.noreply.github.com>
2025-05-06 15:00:58 -0500

    feat: update Summary and Project

--- frontend/src/components/right/Projects.tsx
+++ frontend/src/components/right/Projects.tsx
@@ -1,5 +1,5 @@
 import classes from './styles/Projects.module.css';
-import { Flex, Text, Pill, Image } from '@mantine/core';
+import { Flex, Text, Pill, Image, Tooltip } from '@mantine/core';
 import { projects } from './data/projects.ts';
 import { IconArrowUpRight } from '@tabler/icons-react';
 import { useMediaQuery } from '@mantine/hooks';
@@ -9,49 +9,54 @@ export function Projects() {
 
   const ProjectsList = projects.map((proj, idx) => {
     return (
-      <Flex
-        id='projects'
-        key={idx}
-        mb={25}
-        p={10}
-        className={classes.paper}
-        onMouseDown={(e) => {
-          e.preventDefault();
-          window.open(proj.link, '_blank')
-        }}
-      >
-        <Flex align='start' gap='md' className={classes.inner}>
+      <Tooltip label="No link available for this project" position='bottom' disabled={proj.link !== ''}>
+        <Flex
+          id='projects'
+          key={idx}
+          mb={50}
+          p={10}
+          className={classes.paper}
+          style={{ cursor: proj.link !== '' ? 'pointer' : 'default' }}
+          onMouseDown={(e) => {
+            e.preventDefault();
+            if (proj.link !== '') {
+              window.open(proj.link, '_blank');
+            }
+          }}
+        >
+          <Flex align='start' gap='md' className={classes.inner}>
+            <Flex className={classes.left} direction='column' gap={10} align='center'>
+              <Image
+                src={proj.image}
+                alt={proj.name}
+                mih={100}
+                maw={150}
+                radius="md"
+                bg={'white'}
+              />
+              <Text c="dimmed" fz={13}>{proj.year}</Text>
+            </Flex>
 
-          <Flex className={classes.left} direction='column' gap={10} align='center'>
-            <Image
-              src={proj.image}
-              alt={proj.name}
-              w={150}
-              h={100}
-              radius="md"
-            />
-            <Text c="dimmed" fz={13}>{proj.year}</Text>
-          </Flex>
-
-          <Flex className={classes.right} direction='column'>
-            <Text fz={18} fw={700}>{proj.name} <IconArrowUpRight size={16} className={classes.icon} /></Text>
-            <Text fz={14}>{proj.description}</Text>
-            <Flex gap={10} m={10}>
-              {proj.tools.map((tool, idx) => {
-                return (
-                  <Pill
-                    key={idx}
-                    fz={isMobile ? 12 : 15}
-                    c={'#A3B0BE'}
-                    bg={'#202C39'}
-                    className={classes.pill}
-                  >{tool}</Pill>
-                )
-              })}
+            <Flex className={classes.right} direction='column'>
+              <Text fz={18} fw={700}>{proj.name} {proj.link != '' ? <IconArrowUpRight size={16} className={classes.icon} /> : <></>}</Text>
+              <Text fz={14}>{proj.description}</Text>
+              <Flex gap={10} m={10}>
+                {proj.tools.map((tool, idx) => {
+                  return (
+                    <Pill
+                      key={idx}
+                      fz={isMobile ? 12 : 15}
+                      c={'#A3B0BE'}
+                      bg={'#202C39'}
+                      className={classes.pill}
+                    >{tool}</Pill>
+                  )
+                })}
+              </Flex>
             </Flex>
           </Flex>
         </Flex>
-      </Flex>
+      </Tooltip>
     )
   })
 
--- frontend/src/components/right/Summary.tsx
+++ frontend/src/components/right/Summary.tsx
@@ -4,34 +4,13 @@ export function Summary() {
   return (
     <div className={classes.content}>
       <p>
-        I am a software engineer with enthusiasm for
-        building efficient and scalable applications.
-        My passion for technology and software development
-        comes from my desire to make things faster and easier
-        for both users and other developers.
-      </p>
-
-      <p>
-        I am currently seeking a full-time role in the software engineering field.
-        I am highly motivated by the potential of software to {" "}
-        <span className={classes.highlight}>solve real-world problems</span>.
-        I thrive in collaborative environments where I can work alongside
-        other talented individuals to {" "}
-        <span className={classes.highlight}>develop solutions </span>
-        that enhance user experiences. My adaptability and eagerness to
-        learn enable me to quickly grasp new technologies and contribute
-        effectively to a variety of projects.
+        I’m driven by a desire to create efficient, scalable applications that simplify tasks for users and developers. I’m seeking a full-time role where I can collaborate with others to build solutions that address <span className={classes.highlight}>real-world challenges</span> and <span className={classes.highlight}>improve user experiences</span>. My adaptability and eagerness to learn help me quickly pick up new technologies and contribute to diverse projects.
       </p>
 
       <p>
         In the past, I worked as a developer at{" "}
         <span className={classes.highlight}>
-          <a
-            className={classes.link}
-            href="#experience"
-            target="_parent"
-            rel="noreferrer"
-          >
+          <a className={classes.link} href="#experience" target="_parent" rel="noreferrer">
             National Indemnity Company
           </a>
         </span>
@@ -40,19 +19,14 @@ export function Summary() {
         my technical skills and gained valuable experience utilizing{" "}
         <span className={classes.highlight}>SQL, C#, VB.Net, & Microsoft Azure</span>.
         Beyond this, I also have a background in development
-        with <span className={classes.highlight}> Java, Python, Go, & React</span>.
+        with <span className={classes.highlight}> Java, Go, Python, & React</span>.
       </p>
 
       <p>
-        When I am not coding, I enjoy spending time with my family, snowboarding,
-        playing video games with friends, solving puzzles, and{" "}
+        When I am not coding, I enjoy spending time with my family, snowboarding, playing guitar, solving puzzles, and{" "}
         <span>
-          <a
-            className={classes.link}
-            href="https://duolingo.com/profile/jackkieny"
-            target="_blank"
-            rel="noreferrer"
-          > learning new languages
+          <a className={`${classes.link} ${classes.highlight}`} href="https://duolingo.com/profile/jackkieny" target="_blank" rel="noreferrer">
+            learning new languages
           </a>
         </span>.
       </p>
